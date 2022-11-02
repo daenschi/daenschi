2ebcf8ea9c10a68076125cef20f63539c96ca90b
daenschi <dg@giis.ch>
2022-11-02 11:11:51 +0100

    Remove old Content

--- PS_Course/ps_course.ps1
+++
@@ -1,7 +0,0 @@
-function Start-AutostartServicesThatArentRunning{
-    Get-Service | 
-        Where-Object {$_.StartType -eq "Automatic" -and $_.Status -eq "Stopped"} |
-            ForEach-Object {$_.Start()}
-}
-
-Start-AutostartServicesThatArentRunning
